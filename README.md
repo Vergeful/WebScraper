c5dab97e2b581c240c521541cf9ba6df0ac38297
Savitur Maharaj <saviturmaharaj@gmail.com>
2023-07-30 20:56:59 -0600

    Created loop

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # WebScraper
 This is a python script that uses the Beautiful Soup package to parse Newegg to obtain prices for the Nvidia 4000 series graphics cards.
+
+The csv file was exported to excel to better view the data. Some of the table generated can be seen below:
+
+![Screenshot](stats.png)
--- script.py
+++ script.py
@@ -6,25 +6,72 @@ webpage= requests.get(url)
 document = BeautifulSoup(webpage.text, "html.parser")
 
 # Find how many pages there are:
-# pages = document.find(class_="list-tool-pagination-text").strong #<strong>1<!-- -->/<!-- -->6</strong>
+pages = document.find(class_="list-tool-pagination-text").strong #<strong>1<!-- -->/<!-- -->6</strong>
 
 #Split string to obtain: <strong>1<!-- --> , <!-- -->6<, strong>
 # [1] obtains <!-- -->6<
 # split(">")[-1][:-1] obtains 6
-# number_of_pages = int(str(pages).split("/")[1].split(">")[-1][:-1])
+number_of_pages = int(str(pages).split("/")[1].split(">")[-1][:-1])
 
-# for page in range(1, number_of_pages+1):
-#     url = f'https://www.newegg.ca/p/pl?d=graphics+cards&N=601408872%20100007708&page={page}'
-#     webpage= requests.get(url)
-#     document = BeautifulSoup(webpage.text, "html.parser")
 
-#     gpus = document.find_all(class_="item-container")
+#Begin writing to csv file:
+stats = "stats.csv"
+f = open(stats, "a")
+categories = "Brand, Description, Cost, Rating\n"
+f.write(categories)
 
-# categories = "Brand, Description, Cost, Rating\n"
+def record_gpus(gpus):
+    for gpu in gpus:
+        try:
+            brand = (((gpu.a).next_sibling).div.a.img)["title"]
+        except:
+            try:
+                brand = (((((gpu.a).next_sibling).div).next_sibling).a.img)["title"]
+            except:
+                pass
+        
+        
+        description = (((gpu.a).next_sibling).div).next_sibling.text
+        if(len(description) <= 7):
+            description = ((((gpu.a).next_sibling).div).next_sibling).next_sibling.text
 
-gpu = document.find_all(class_="item-container")[5]
+        price_dollar = (((((gpu.a).next_sibling).next_sibling).ul.li).next_sibling).next_sibling.strong.text
+        price_cents = (((((gpu.a).next_sibling).next_sibling).ul.li).next_sibling).next_sibling.sup.text
+        price = "$" + price_dollar + price_cents
 
-f = open("lol.html", "a")
+
+        try:
+            rating = ((((((gpu.a).next_sibling).div).next_sibling).a).next_sibling)["title"].split("+")[-1].strip()
+        except:
+            try:
+                rating = (((((gpu.a).next_sibling).div).a).next_sibling)["title"].split("+")[-1].strip()
+            except:
+                pass
+        
+        try:
+            gpu_stats = brand.replace(",", " ") + "," + description.replace(",", " ") + "," + price.replace(",", " ") + "," + rating + "\n"
+            f.write(gpu_stats)
+        except:
+            pass
+   
+           
+for page in range(1, number_of_pages+1):
+    url = f'https://www.newegg.ca/p/pl?d=graphics+cards&N=601408872%20100007708&page={page}'
+    webpage= requests.get(url)
+    document = BeautifulSoup(webpage.text, "html.parser")
+
+    gpus = document.find_all(class_="item-container")
+
+    record_gpus(gpus)
+
+f.close()
+
+
+
+
+
+# gpu = document.find_all(class_="item-container")[5]
+# f = open("stats.csv", "a")
 
 #Brand: 
 # Try except block used if container has lowest price div:
