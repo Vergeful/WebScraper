540af1bc2d869dab01555a80a23bd4d8ea735bdd
Savitur Maharaj <saviturmaharaj@gmail.com>
2023-07-30 23:50:43 -0600

    Removed bugs

--- script.py
+++ script.py
@@ -22,40 +22,27 @@ f.write(categories)
 
 def record_gpus(gpus):
     for gpu in gpus:
-        try:
-            brand = (((gpu.a).next_sibling).div.a.img)["title"]
-        except:
-            try:
-                brand = (((((gpu.a).next_sibling).div).next_sibling).a.img)["title"]
-            except:
-                pass
-        
-        
-        # If the container has the lowest price div, the description is number of ratings i.e. (22).
-        # Assume that this string is <= 7 characters
-        description = (((gpu.a).next_sibling).div).next_sibling.text
-        if(len(description) <= 7):
-            description = ((((gpu.a).next_sibling).div).next_sibling).next_sibling.text
+       # Some brands are not listed:
+       try:
+           brand = (gpu.find_all(class_="item-brand"))[0].img["title"]
+       except:
+           brand = "N/A"
+       
+       description = (gpu.find_all(class_="item-title"))[0].text
 
-        price_dollar = (((((gpu.a).next_sibling).next_sibling).ul.li).next_sibling).next_sibling.strong.text
-        price_cents = (((((gpu.a).next_sibling).next_sibling).ul.li).next_sibling).next_sibling.sup.text
-        price = "$" + price_dollar + price_cents
-
-
-        try:
-            rating = ((((((gpu.a).next_sibling).div).next_sibling).a).next_sibling)["title"].split("+")[-1].strip()
-        except:
-            try:
-                rating = (((((gpu.a).next_sibling).div).a).next_sibling)["title"].split("+")[-1].strip()
-            except:
-                pass
-        
-        try:
-            gpu_stats = brand.replace(",", " ") + "," + description.replace(",", " ") + "," + price.replace(",", " ") + "," + rating + "\n"
-            f.write(gpu_stats)
-        except:
-            pass
-   
+       price_dollars = (gpu.find_all(class_="price-current"))[0].strong.text
+       price_cents = (gpu.find_all(class_="price-current"))[0].sup.text 
+       price = "$" + price_dollars + price_cents
+       
+       # Some ratings are not listed
+       try:
+           rating= gpu.find_all(class_="item-rating")[0]["title"].split("+")[-1].strip()
+       except:
+           rating = "N/A"
+       
+       gpu_stats = brand.replace(",", " ") + "," + description.replace(",", " ") + "," + price.replace(",", " ") + "," + rating + "\n"
+       f.write(gpu_stats)
+    
            
 for page in range(1, number_of_pages+1):
     url = f'https://www.newegg.ca/p/pl?d=graphics+cards&N=601408872%20100007708&page={page}'
